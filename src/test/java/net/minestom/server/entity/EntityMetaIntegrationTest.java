mudkipdev/minestom

commit e99ad151879f1c47df7e6fbc5e8abedcd250da9f

Metadata fix (#3089)

src/main/java/net/minestom/server/entity/MetadataHolder.java
@@ -42,6 +42,7 @@ import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.BiFunction;
 
 public final class MetadataHolder {
@@ -86,6 +87,9 @@ public final class MetadataHolder {
 
     public <T> void set(MetadataDef.Entry<T> entry, T value) {
         final int id = entry.index();
+
+        T current = get(entry);
+        if (Objects.equals(current, value)) return;
 
         Metadata.Entry<?> result = switch (entry) {
             case MetadataDef.Entry.Index<T> v -> v.function().apply(value);

src/test/java/net/minestom/server/entity/PlayerSprintingMetadataTest.java
@@ -1,0 +1,36 @@
+package net.minestom.server.entity;
+
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.network.packet.client.play.ClientEntityActionPacket;
+import net.minestom.server.network.packet.client.play.ClientInputPacket;
+import net.minestom.server.network.packet.server.play.EntityMetaDataPacket;
+import net.minestom.testing.Env;
+import net.minestom.testing.EnvTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@EnvTest
+public class PlayerSprintingMetadataTest {
+
+    @Test
+    public void sprintingMetadata(Env env) {
+        var instance = env.createFlatInstance();
+        var connection = env.createConnection();
+        var player = connection.connect(instance, new Pos(0, 40, 0));
+
+        player.addPacketToQueue(new ClientInputPacket(true, false, false, false, false, false, true));
+        player.addPacketToQueue(new ClientEntityActionPacket(
+                player.getEntityId(),
+                ClientEntityActionPacket.Action.START_SPRINTING,
+                0
+        ));
+
+        var tracker = connection.trackIncoming(EntityMetaDataPacket.class);
+        player.interpretPacketQueue();
+
+        var packets = tracker.collect();
+        assertEquals(1, packets.size(), "Expected single packet, got multiple");
+    }
+
+}

commit cd6b1fda3b3f36e4c414088ebfd044ea5c6c78a6

fix: always send display entity interpolation start delta even if unchanged
This fixes a regression caused by #3089 where interpolation start delta is not sent, thus not starting interpolation when expected.

src/main/java/net/minestom/server/entity/MetadataHolder.java
@@ -89,7 +89,14 @@ public final class MetadataHolder {
         final int id = entry.index();
 
         T current = get(entry);
-        if (Objects.equals(current, value)) return;
+
+        // If a metadata value is unchanged we should not send it. In particular we need to be careful with
+        //  sending bitmasks which will overwrite client-predicted values. See PR 3089 for more info.
+        // However, interpolation delay is expected to be sent regularly with the same value to begin
+        //  interpolation so we always send it for now.
+        if (Objects.equals(current, value) && entry != MetadataDef.Display.INTERPOLATION_DELAY) {
+            return;
+        }
 
         Metadata.Entry<?> result = switch (entry) {
             case MetadataDef.Entry.Index<T> v -> v.function().apply(value);

src/test/java/net/minestom/server/entity/EntityMetaIntegrationTest.java
@@ -2,6 +2,7 @@ package net.minestom.server.entity;
 
 import net.kyori.adventure.text.Component;
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.metadata.display.ItemDisplayMeta;
 import net.minestom.server.network.packet.server.play.EntityMetaDataPacket;
 import net.minestom.testing.Env;
 import net.minestom.testing.EnvTest;
@@ -135,5 +136,35 @@ public class EntityMetaIntegrationTest {
             if (entry.type() != Metadata.TYPE_OPT_CHAT) return;
             assertEquals(Component.text("Custom Name 2"), entry.value());
         });
+    }
+
+    @Test
+    public void displayInterpolationDurationAlwaysSend(Env env) {
+        // ensure that display entity interpolation start delta is always sent even if we send the same value repeatedly.
+
+        var connection = env.createConnection();
+        var instance = env.createFlatInstance();
+        var startPos = new Pos(0, 42, 1);
+
+        connection.connect(instance, startPos);
+        var incomingPackets = connection.trackIncoming(EntityMetaDataPacket.class);
+
+        var entity = new Entity(EntityType.ITEM_DISPLAY);
+        entity.setInstance(instance, startPos);
+        var meta = (ItemDisplayMeta) entity.getEntityMeta();
+
+        meta.setTransformationInterpolationStartDelta(1);
+        meta.setTransformationInterpolationStartDelta(2); // same tick
+
+        env.tick();
+        env.tick();
+
+        meta.setTransformationInterpolationStartDelta(3);
+
+        var packets = incomingPackets.collect();
+        assertEquals(4, packets.size()); // the 3 we sent, and 1 more for the spawn
+        assertEquals(1, packets.get(1).entries().get(MetadataDef.Display.INTERPOLATION_DELAY.index()).value());
+        assertEquals(2, packets.get(2).entries().get(MetadataDef.Display.INTERPOLATION_DELAY.index()).value());
+        assertEquals(3, packets.get(3).entries().get(MetadataDef.Display.INTERPOLATION_DELAY.index()).value());
     }
 }